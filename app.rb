adab73d652bd78592a5827874d50a2013f796e22
Jesse Shawl <jesse@jshawl.com>
2016-10-02 10:32:26 -0400

    initial commit

---
+++ app.rb
@@ -0,0 +1,18 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require_relative 'metro'
+
+get '/' do
+  @lines = metro.keys
+  erb :index
+end
+
+get '/start' do
+  @stations = metro[params[:line].to_sym]
+  erb :start
+end
+
+get '/end' do
+  @stations = metro[params[:line].to_sym]
+  erb :end
+end
---
+++ metro.rb
@@ -0,0 +1,7 @@
+def metro
+  {
+    "red" => [
+      "Shady Grove", "Rockville", "Twinbrook", "White Flint", "Grovesnor-Strathmore", "Medical Center", "Bethesda", "Friendship Heights", "Tenelytown-AU", "Van Ness-UDC", "Cleveland Park", "Woodley Park", "Dupont Circle", "Faragut North", "Metro Center", "Gallery Place", "Judiciary Square", "Union Station", "NOMA-Gallaudet U", "Rhode Island Ave", "Brookland-CUA", "Fort Totten", "Takoma", "Silver Spring", "Forest Glen", "Wheaton", "Glenmont"
+    ]
+  }
+end
