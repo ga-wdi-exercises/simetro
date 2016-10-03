8a6e5abcfdec75a6913f3a4b597f4367e5d4d1e7
Andrew Whitley <awhitley1233@gmail.com>
2016-10-03 18:56:14 -0400

    finished MVP

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require 'sinatra'
 require 'sinatra/reloader'
 require_relative 'metro'
 
+
+
 get '/' do
   @lines = metro.keys
   erb :index
@@ -16,4 +18,12 @@ end
 get '/end' do
   @stations = metro[params[:line].to_sym]
   erb :end
+end
+
+get '/trip' do
+  line = metro[params[:line].to_sym]
+  start = line.index(params[:start])
+  finish = line.index(params[:end])
+  @num_stops = ((finish - start).abs) - 1
+  erb :trip
 end
