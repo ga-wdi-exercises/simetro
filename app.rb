128be760bee29ea3dca26d44a218b7c7da16ba5f
Barrett Cope <barrettcope@gmail.com>
2016-10-03 17:58:25 -0400

    Basic trip functionality working.

--- app.rb
+++ app.rb
@@ -17,3 +17,10 @@ get '/end' do
   @stations = metro[params[:line].to_sym]
   erb :end
 end
+
+get '/trip' do
+  @start = params[:start].to_s
+  @end = params[:end].to_s
+  @num_stops = (metro[params[:line].to_sym].find_index(params[:start].to_s) - metro[params[:line].to_sym].find_index(params[:end].to_s)).abs - 1
+  erb :trip
+end
