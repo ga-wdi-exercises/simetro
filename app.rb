c43ac729c3a208223a37a7122690d17eda1e4ec2
Alex Manno <alman92@gmail.com>
2016-10-04 18:33:00 -0400

    Stop counter working

--- app.rb
+++ app.rb
@@ -17,3 +17,14 @@ get '/end' do
   @stations = metro[params[:line].to_sym]
   erb :end
 end
+
+get '/trip' do
+  #bring array of a line in
+  trip = metro[params[:line].to_sym]
+  #designate a start and finish
+  start = trip.index(params[:start])
+  finish = trip.index(params[:end])
+  #math for number of stops probably goes here (finish-start)
+  @num_stops = (finish - start).abs
+  erb :trip
+end
