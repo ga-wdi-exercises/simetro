e89f4d41c6ee38cf4fc24a072339406e096a909e
William Keating <keating49@gmail.com>
2016-10-06 07:14:28 -0400

    trip.erb

--- app.rb
+++ app.rb
@@ -17,3 +17,12 @@ get '/end' do
   @stations = metro[params[:line].to_sym]
   erb :end
 end
+
+get '/trip' do
+  line = metro[params[:line].to_sym]
+  start = line.index(params[:start])
+  finish = line.index(params[:finish])
+  trip = (finish - start).abs
+  erb :trip
+#
+end
