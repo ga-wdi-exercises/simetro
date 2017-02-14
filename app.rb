b2ec59d1b0c5360748446bbebb59ca9ab2b352a1
Eric Christopher Green <VERDE611@GMAIL.COM>
2017-02-13 20:35:08 -0500

    variables set

--- app.rb
+++ app.rb
@@ -17,3 +17,9 @@ get '/end' do
   @stations = metro[params[:line].to_sym]
   erb :end
 end
+
+get '/trip' do
+  line = metro[params[:line].to_sym]
+  last = line.index(params[:end])
+  start = line.index(params[:start])
+end
