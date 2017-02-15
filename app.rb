31e999aeea491d1e40ba5425fc1656273468cd9d
Fiona <h.natapra@gmail.com>
2017-02-15 12:32:09 -0500

    final commit - added trip.erb

--- app.rb
+++ app.rb
@@ -17,3 +17,15 @@ get '/end' do
   @stations = metro[params[:line].to_sym]
   erb :end
 end
+
+get '/trip' do
+     # to_sym converts a string to a symbol:
+    line = metro[params[:line].to_sym]
+    # using .index, set the starting station from :line :
+    start = line.index(params[:start])
+    # set the end station from :line :
+    last_station = line.index(params[:end])
+    # get number of stops:
+  @num_stops = ((last_station - start).abs).to_s
+  erb :trip
+end
