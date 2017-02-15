f5468976017de7c6df156bb10e919d57dd5211ae
Elena Santana <fanelenan7@gmail.com>
2017-02-15 10:39:54 -0500

    simetro submit

--- app.rb
+++ app.rb
@@ -7,6 +7,14 @@ get '/' do
   erb :index
 end
 
+get '/trip' do
+  line = metro[params[:line].to_sym]
+  start_station = line.index(params[:start])
+  end_station = line.index(params[:end])
+  @num_stops = ((start_station - end_station).abs).to_s
+  erb :trip
+end
+
 get '/start' do
   params[:line]
   @stations = metro[params[:line].to_sym]
