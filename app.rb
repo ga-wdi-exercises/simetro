eaf7f1ec2efbde6248b928254522fb06221ffb8f
Samuel Clements <sdkclements@gmail.com>
2017-02-15 17:10:41 -0500

    now i did

--- app.rb
+++ app.rb
@@ -7,13 +7,18 @@ get '/' do
   erb :index
 end
 
+get '/trips' do
+	@num_stop = @staions[0]
+
 get '/start' do
   params[:line]
   @stations = metro[params[:line].to_sym]
+  @startStation = metro[params[:line]]
   erb :start
 end
 
 get '/end' do
   @stations = metro[params[:line].to_sym]
+  @endStation = metro[params[:line]]
   erb :end
 end
