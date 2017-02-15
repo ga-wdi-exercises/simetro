b33783e0943409b9abb8fe098d8cced1f5b6bae4
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-02-15 09:34:08 -0500

    added trip functionality

--- app.rb
+++ app.rb
@@ -17,3 +17,11 @@ get '/end' do
   @stations = metro[params[:line].to_sym]
   erb :end
 end
+
+get '/trip' do
+  line = metro[params[:line].to_sym]
+  beginning_line = line.index(params[:start])
+  destination_line = line.index(params[:end])
+  @num_stops = (destination_line - beginning_line).abs
+  erb :trip
+end
