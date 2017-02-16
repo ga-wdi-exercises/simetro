778193fcbcbf2d88dce271a8c92baca111b884a6
Dominick Houston <dominick.houston14@gmail.com>
2017-02-15 23:07:01 -0500

    Complete

--- app.rb
+++ app.rb
@@ -17,3 +17,12 @@ get '/end' do
   @stations = metro[params[:line].to_sym]
   erb :end
 end
+
+get "/trip" do
+  metro_line = metro[params[:line].to_sym]
+  start = metro_line.index(params[:start])
+  stop = metro_line.index(params[:end])
+  @num_stops = ((stop - start).abs).to_s
+  erb :trip
+
+end
