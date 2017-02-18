d3228fa8d9c98496588af96b32cd11af2039594c
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-02-17 21:03:54 -0500

    views update

--- app.rb
+++ app.rb
@@ -2,20 +2,17 @@ require 'sinatra'
 require 'sinatra/reloader'
 require_relative 'metro'
 
-get '/' do
-  @lines = metro.keys
-  erb :index
+get '/trip' do
+  lines = metro[params[:line].to_sym]
+  puts "*" *50
+  puts line
+  start_location = params[:start]
+  start_index = line.index start_location
+  puts start_index
+  end_location = params[:end]
+  end_index = line.index end_location
+  puts "*" 50
+  puts end_index
+  @num_stops
+  erb :trip
 end
-
-get '/start' do
-  params[:line]
-  @stations = metro[params[:line].to_sym]
-  erb :start
-end
-
-get '/end' do
-  @stations = metro[params[:line].to_sym]
-  erb :end
-end
-
-still working on this . . .
