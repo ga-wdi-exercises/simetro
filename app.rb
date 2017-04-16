7e5badae4245a1051d951e256d269c5bb22f9429
meagdoh <meagdoherty@gmail.com>
2017-04-16 13:01:46 -0400

    Initial commit. Add trip.erb

--- app.rb
+++ app.rb
@@ -2,18 +2,27 @@ require 'sinatra'
 require 'sinatra/reloader'
 require_relative 'metro'
 
+
 get '/' do
   @lines = metro.keys
   erb :index
 end
-
+#
 get '/start' do
   params[:line]
   @stations = metro[params[:line].to_sym]
   erb :start
 end
-
+#
 get '/end' do
   @stations = metro[params[:line].to_sym]
   erb :end
+end
+
+
+get '/trip' do
+  #  "Starting at #{params[:start]} and ending at #{params[:end]}"
+  #  @start_stop = params[:start].to_sym)
+  #  @end_stop =
+  erb :trip
 end
