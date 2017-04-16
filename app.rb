0b470f55f7a1ec536f5a9662774bb844375ab85e
Whitney <whitney.purdum@gmail.com>
2017-04-16 16:46:06 -0400

    updates

--- app.rb
+++ app.rb
@@ -3,30 +3,32 @@ require 'sinatra/reloader'
 require_relative 'metro'
 
 
-
+# params =
+#   {:mode => mode, :id => id, :new => new}
 
 get '/' do
   @lines = metro.keys
-  @chosen_line = params[:line]
-  @start_index = metro.index(@chosen_line)
-  puts @start_index
-  @start_station = params[:start]
   erb :index
 end
 
 get '/start' do
+  @chosen_line = params[:line]
+  @remove_quotes = params[:line].chomp('"').reverse.chomp('"').reverse
+  @chosen_line_complete = metro[:@remove_quotes]
+  puts "This is the line: #{@chosen_line}"
+  puts "What is this? #{@chosen_line_complete}"
+  @start_station = params[:start]
   @stations = metro[params[:line].to_sym]
-  @start_station_index = metro.start_index.index(@start_station)
+  # @start_station_index = metro.start_index.index(@start_station)
   erb :start
 end
 # #
-# get '/end' do
-#   @stations = metro[params[:line].to_sym]
-#   @end_station = params[:end]
-#   @end_station_index = metro.start_index.index(@end_station)
-#
-#   erb :end
-# end
+get '/end' do
+  @stations = metro[params[:line].to_sym]
+  puts "This is the start station #{@start_station}"
+  @end_station = params[:end]
+  erb :end
+end
 # #
 # get '/trip' do
 #   @num_stops = @end_station_index - @start_station_index
