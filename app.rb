8956d03f724f080c7ee2671b92bbda8d021139b6
Jeong Hoon Koo <junghoone17@gmail.com>
2017-04-16 22:54:57 -0400

    step 2 & 3 complete

--- app.rb
+++ app.rb
@@ -17,3 +17,15 @@ get '/end' do
   @stations = metro[params[:line].to_sym]
   erb :end
 end
+
+
+get '/trip' do
+  @line = params[:line]
+  @start_station = params[:start]
+  @start_station_index = metro[@line.to_sym].index("#{@start_station}")
+  @end_station = params[:end]
+  @end_station_index = metro[@line.to_sym].index("#{@end_station}")
+  num_stops = (@start_station_index - @end_station_index).abs
+  @num_stops = num_stops.to_s
+  erb :trip
+end
