7ea499723bd808a55d17e54cc9a81ffef91d0e29
Matt Caplan <mattpcaplan@gmail.com>
2017-04-16 21:13:35 -0400

    This should be up and running

--- app.rb
+++ app.rb
@@ -7,8 +7,12 @@ get '/' do
   erb :index
 end
 
+get '/' do
+  1
+end
+
 get '/start' do
-  params[:line]
+  @line = params[:line]
   @stations = metro[params[:line].to_sym]
   erb :start
 end
@@ -16,4 +20,24 @@ end
 get '/end' do
   @stations = metro[params[:line].to_sym]
   erb :end
+end
+
+get '/trip' do
+# puts "hello world"
+  line = metro[params[:line].intern]
+  first_station = params[:start]
+  first_index = line.find_index(first_station)
+
+  last_station = params[:end]
+  last_index = line.find_index(last_station)
+
+#trying to ensure that we get the number of stops and not get negative stops.  So we choose the larger of the two(first index vs last index) and subtract the smaller from the larger
+stops =
+  last_index - first_index if last_index > first_index
+  first_index - last_index if last_index < first_index
+
+
+@num_stops = stops.to_s
+  # return first_index.to_s
+  erb :trips
 end
--- metro.rb
+++ metro.rb
@@ -1,3 +1,17 @@
+# def metro
+#   {
+#     "red": [
+#       "Shady Grove", "Rockville", "Twinbrook", "White Flint", "Grovesnor-Strathmore", "Medical Center", "Bethesda", "Friendship Heights", "Tenelytown-AU", "Van Ness-UDC", "Cleveland Park", "Woodley Park", "Dupont Circle", "Faragut North", "Metro Center", "Gallery Place", "Judiciary Square", "Union Station", "NOMA-Gallaudet U", "Rhode Island Ave", "Brookland-CUA", "Fort Totten", "Takoma", "Silver Spring", "Forest Glen", "Wheaton", "Glenmont"
+#     ],
+#     "silver": ["Wiehle-Reston East","Spring Hill","Greensboro","Tysons Corner","McLean","East Falls Church","Ballston-MU","Virginia Sq-GMU","Clarendon","Court House","Rosslyn","Foggy Bottom-GWU","Farragut West","McPherson Sq","Metro Center","Federal Triangle","Smithsonian","L'enfant Plaza","Federal Center SW","Capitol South","Eastern Market","Potomac Ave","Stadium-Armory","Benning Rd","Capitol Heights","Addison Rd","Morgan Blvd","Largo Town Center"],
+#     "blue": ["Franconia–Springfield", "Van Dorn Street", "King Street – Old Town", "Braddock Road", "Potomac Yard", "Ronald Reagan Washington National Airport", "Crystal City", "Pentagon City", "Pentagon", "Arlington Cemetery", "Rosslyn", "Foggy Bottom – GWU", "Farragut West", "McPherson Square", "Metro Center", "Federal Triangle", "Smithsonian", "L'Enfant Plaza", "Federal Center SW", "Capitol South", "Eastern Market", "Potomac Avenue", "Stadium–Armory", "Benning Road", "Capitol Heights", "Addison Road", "Morgan Boulevard", "Largo Town Center", "     Future station"],
+#     "orange": ["Vienna", "Dunn Loring", "West Falls Church", "East Falls Church", "Ballston–MU", "Virginia Square–GMU", "Clarendon", "Court House", "Rosslyn", "Foggy Bottom–GWU", "Farragut West", "McPherson Square", "Metro Center", "Federal Triangle", "Smithsonian", "L'Enfant Plaza", "Federal Center SW", "Capitol South", "Eastern Market", "Potomac Avenue", "Stadium–Armory", "Minnesota Avenue", "Deanwood", "Cheverly", "Landover", "New Carrollton"],
+#     "yellow": ["Huntington", "Eisenhower Avenue", "King Street – Old Town", "Braddock Road", "Potomac Yard", "Ronald Reagan Washington National Airport", "Crystal City", "Pentagon City", "Pentagon", "L'Enfant Plaza", "Archives – Navy Memorial – Penn Quarter", "Gallery Place", "Mount Vernon Square", "Shaw – Howard University", "U Street / African-American Civil War Memorial / Cardozo", "Columbia Heights", "Georgia Avenue – Petworth", "Fort Totten", "West Hyattsville", "Prince George's Plaza", "College Park – University of Maryland", "Greenbelt"],
+#     "green": ["Branch Avenue", "Suitland", "Naylor Road", "Southern Avenue", "Congress Heights", "Anacostia", "Navy Yard – Ballpark", "Waterfront", "L'Enfant Plaza", "Archives", "Gallery Place", "Mount Vernon Square", "Shaw – Howard University", "U Street", "Columbia Heights", "Georgia Avenue – Petworth", "Fort Totten", "West Hyattsville", "Prince George's Plaza", "College Park – University of Maryland", "Greenbelt"]
+#   }
+# end
+
+
 def metro
   {
     "red": [
