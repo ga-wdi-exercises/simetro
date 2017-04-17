a27103dadb78e3eeca7e4cf6d8714cad7f839734
Marcus Linn Miller Jr <marcusmiller220@gmail.com>
2017-04-17 00:57:40 -0400

    step 3

--- app.rb
+++ app.rb
@@ -3,17 +3,31 @@ require 'sinatra/reloader'
 require_relative 'metro'
 
 get '/' do
-  @lines = metro.keys
-  erb :index
+    @lines = metro.keys
+    erb :index
 end
 
 get '/start' do
-  params[:line]
-  @stations = metro[params[:line].to_sym]
-  erb :start
+    params[:line]
+    @stations = metro[params[:line].to_sym]
+    erb :start
 end
 
 get '/end' do
-  @stations = metro[params[:line].to_sym]
-  erb :end
+    @stations = metro[params[:line].to_sym]
+erb :end
 end
+
+get '/trip' do
+    line = metro[params[:line].to_sym]
+    start_station = params[:start]
+    start_index = line.find_index(start_station)
+end_station = params[:end]
+end_index = line.find_index(end_station)
+stops = end_index - start_index if end_index > start_index
+@num_stops = stops.to_s
+erb :trip
+end
+
+puts @start_station
+puts @end_station
