eea190dd7fadb9a9e846729032fc28b859fdd795
Caroline Chen <carolinecaroline@gmail.com>
2017-04-17 14:31:02 -0400

    got help and completed hw

--- app.rb
+++ app.rb
@@ -20,6 +20,21 @@ end
 
 get '/trip' do
 	# for testing, just putting in 3 for now.  how to get index of the start station and end station?
-	@num_stops = "3"
-	erb :trip
+	# @num_stops = "3"
+
+#Jon showed me how to do this
+
+line = metro[params[:line].to_sym]
+start_station = params[:start]
+start_index = line.find_index(start_station)
+
+end_station = params[:end]
+end_index = line.find_index(end_station)
+
+stops = end_index - start_index if end_index > start_index
+stops = start_index - end_index if end_index < start_index
+
+@num_stops = stops.to_s
+
+  erb :trip
 end
