b4e75574416dc0252ffe820701ed5d551097e3c8
Danielle Fricke <danielle.fricke11@gmail.com>
2017-05-29 18:19:08 -0400

    css and trip.erb completed

--- app.rb
+++ app.rb
@@ -17,3 +17,8 @@ get '/end' do
   @stations = metro[params[:line].to_sym]
   erb :end
 end
+
+get '/trip' do
+  @num_stops = metro[params[:start].to_sym] - metro[params[:end].to_sym]
+  erb :trip
+end
