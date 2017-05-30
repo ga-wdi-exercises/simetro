532cf81085638211df8790bb3bc5de171aba9a76
tedlayea <tedlayea@gMAIL.com>
2017-05-30 08:45:04 -0400

    simetro modified

--- app.rb
+++ app.rb
@@ -4,16 +4,23 @@ require_relative 'metro'
 
 get '/' do
   @lines = metro.keys
+  chosenLine= params[:line]
   erb :index
 end
 
 get '/start' do
   params[:line]
-  @stations = metro[params[:line].to_sym]
   erb :start
+  @stations = metro[params[:line].to_sym]
+  @startingstation= chosenLine.index(@stations).to_i
 end
 
 get '/end' do
   @stations = metro[params[:line].to_sym]
+  @finalstation=chosenLine.index(@stations).to_i
   erb :end
+end
+get '/trip' do
+  erb :trip
+  @num_stops=@startingstation-@finalstation
 end
