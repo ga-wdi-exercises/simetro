e917bed872d7c674d3b3e6249d70a850efdb553e
Jessa Marie Barre <jmlbarre@gmail.com>
2017-07-24 14:13:33 -0400

    grabbed query parameters and calculated result

--- app.rb
+++ app.rb
@@ -7,6 +7,11 @@ get '/' do
   erb :index
 end
 
+# get '/' do
+#   1 # must be "1"
+# end
+
+
 get '/start' do
   params[:line]
   @stations = metro[params[:line].to_sym]
@@ -16,4 +21,17 @@ end
 get '/end' do
   @stations = metro[params[:line].to_sym]
   erb :end
+end
+
+get '/trip' do
+start_trip = params[:start]
+end_trip = params[:end]
+line = params[:line].to_sym
+
+start_index = metro[line].find_index(start_trip)
+end_index = metro[line].find_index(end_trip)
+
+num_stops = end_index - start_index
+return num_stops.to_s
+
 end
