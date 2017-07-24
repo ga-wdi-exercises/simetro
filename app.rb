a7bb2f73ae71c1cf794c69b63b0d1899665dacc1
Jonathan Polakoff <jdpolakoff@gmail.com>
2017-07-24 14:27:34 -0400

    calculated distance

--- app.rb
+++ app.rb
@@ -17,3 +17,19 @@ get '/end' do
   @stations = metro[params[:line].to_sym]
   erb :end
 end
+
+# get '/start/:start_station' do
+#   @start_station = params[:start_station]
+# end
+#
+# get '/end/:end_station' do
+#   @end_station = params[:end_station]
+# end
+
+get '/trip' do
+    stations = metro[params[:line].to_sym]
+    first = stations.index(params[:start])
+    last = stations.index(params[:end])
+    distance = first - last
+    return "#{distance}"
+end
