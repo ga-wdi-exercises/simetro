60fdfe08e1bbb9f46298a88eaa3f00eb61df3812
Jacob Greenwald <jeg4t@virginia.edu>
2017-07-24 14:24:08 -0400

    basic functionality

--- app.rb
+++ app.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require 'sinatra'
 require 'sinatra/reloader'
 require_relative 'metro'
@@ -8,8 +9,8 @@ get '/' do
 end
 
 get '/start' do
-  params[:line]
-  @stations = metro[params[:line].to_sym]
+  line = params[:line]
+  @stations = metro[line.to_sym]
   erb :start
 end
 
@@ -17,3 +18,23 @@ get '/end' do
   @stations = metro[params[:line].to_sym]
   erb :end
 end
+
+# get '/start/:start_station' do
+#   @start_station = metro[params[:start_station].to_sym]
+#   # return "#{start_station}"
+# end
+
+get '/trip' do
+  stations = metro[params[:line].to_sym]
+  first = stations.index(params[:start])
+  last = stations.index(params[:end])
+  distance = last - first
+  if distance < 0
+  return "#{distance * -1}"
+else
+  return "#{distance}"
+end
+end
+
+# binding.pry
+# puts "end of file"
