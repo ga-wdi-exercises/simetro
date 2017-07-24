61a6233d850c9cd1756d0fac43698fc93cea99e2
Jonathan Polakoff <jdpolakoff@gmail.com>
2017-07-24 17:26:50 -0400

    added negative distance functionality;

--- app.rb
+++ app.rb
@@ -18,18 +18,14 @@ get '/end' do
   erb :end
 end
 
-# get '/start/:start_station' do
-#   @start_station = params[:start_station]
-# end
-#
-# get '/end/:end_station' do
-#   @end_station = params[:end_station]
-# end
-
 get '/trip' do
     stations = metro[params[:line].to_sym]
     first = stations.index(params[:start])
     last = stations.index(params[:end])
     distance = first - last
+    if distance > 0
     return "#{distance}"
+    else
+    return "#{distance.to_i * -1}"
+  end
 end
