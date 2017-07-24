27cc86c68e8f37632aa6c717e6f27cccc2684ae4
Liz Jewell <eghjewell@gmail.com>
2017-07-24 18:17:08 -0400

    Homework Steps 1-3 (no bonus yet)

--- app.rb
+++ app.rb
@@ -7,6 +7,14 @@ get '/' do
   erb :index
 end
 
+get '/trip' do
+  @stations = metro[params[:line].to_sym]
+  @start_station = @stations.index(params[:start]).to_i
+  @end_station = @stations.index(params[:end]).to_i
+  @num_stops = @end_station - @start_station
+  erb :trip
+end
+
 get '/start' do
   params[:line]
   @stations = metro[params[:line].to_sym]
