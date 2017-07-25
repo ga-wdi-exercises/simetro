521d204571f58b1df8ea514d28fb6b4582a44460
Josue Rosales <jhosue.rosales@gmail.com>
2017-07-24 21:25:23 -0400

    substracted -1 to the num_stops because between stops should only be counted

--- app.rb
+++ app.rb
@@ -5,9 +5,11 @@ require_relative 'metro'
 get '/' do
   @lines = metro.keys
   erb :index
+
 end
 
 get '/start' do
+  puts "hi over there"
   params[:line]
   @stations = metro[params[:line].to_sym]
   erb :start
@@ -15,5 +17,26 @@ end
 
 get '/end' do
   @stations = metro[params[:line].to_sym]
+  puts params[:start]
   erb :end
+end
+
+
+get '/trip' do
+
+ @stations = metro[params[:line].to_sym]
+
+ @pos1 =params[:start]
+ @pos2 =params[:end]
+
+
+@initial = @stations.find_index(@pos1)
+
+
+@final = @stations.find_index(@pos2)
+
+@num_stops = @final - @initial
+
+erb :trip
+
 end
