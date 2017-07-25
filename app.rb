ba8bf16457a4e45886269d2f2d87064c26f3a7dc
Charles Cox <cox.charles.w@gmail.com>
2017-07-24 20:04:04 -0400

    station count works

--- app.rb
+++ app.rb
@@ -19,24 +19,10 @@ get '/end' do
 end
 
 get '/trip' do
-  # @start_index = @line.find_index(@start)
-  # @stop_index = @line.find_index(@end)
-  # # stop_index = metro.find_index{|x| x(line) == end_of_line}
-  # @num_stops = abs(start_index - stop_index)
   @start = params[:start]
   @stop = params[:end]
- # line = params[:line].to_sym
   @start_index = metro[params[:line].to_sym].find_index(@start)
   @stop_index = metro[params[:line].to_sym].find_index(@stop)
-  @num_stops = (@stop_index.to_i - @start_index.to_i).abs.to_s
-  # @num_stops = @num_stops.to_s
- erb :trip
+  @num_stops = (@stop_index.to_i - @start_index.to_i).abs
+  erb :trip
 end
-
-# get '/trip' do
-#   stations = metro[params[:line].to_sym]
-#  first = stations.index(params[:start])
-#  last = stations.index(params[:end])
-#  @distance = last.to_i - first.to_i
-#  erb :trip
-# end
